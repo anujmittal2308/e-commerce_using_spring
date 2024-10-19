bbdbdc825373adc588f6adf32ed6e7baa692626a
anujmittal2308 <anujmittal2308@gmail.com>
2024-10-20 01:39:05 +0530

    setup git

---
+++ src/test/java/com/e_comm/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_comm.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
